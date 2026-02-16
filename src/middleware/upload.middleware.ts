1e33336758fc9ac19d993b9e592a0d42deb1f7c6
sagargohil07 <80506662+sagargohil07@users.noreply.github.com>
2026-02-16 15:05:50 +0530

    Update upload.middleware.ts

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -7,8 +7,8 @@ const storate = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
   },
 });
 
